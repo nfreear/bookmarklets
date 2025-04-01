5bfd6bb1062f947e8394058ea38d555ce70add6a
Nick Freear <ndf@Nicks-MBP.broadband>
2025-04-01 21:19:36 +0100

    task: Add lint and deploy workflow (#1)

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+export { youtrackRecheckListBookmarklet } from './lib/youtrackRecheckList.marklet.js';
 export { youtrackRecheckList } from './lib/youtrackRecheckList.js';
 export { youtrackRecheckListBookmarkletLegacy } from './lib/youtrackRecheckList.legacy.js';
 export { youtrackRecheckListBookmarkletPSpeller } from './lib/youtrackRecheckList.PS.js';
