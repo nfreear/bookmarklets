7731673509734386ffc282dd3bd2bf6fe4e3beb2
Nick Freear <ndf@Nicks-MBP.broadband>
2025-04-01 17:43:52 +0100

    task: YouTrack bookmarklet, version 2 (#1)

---
+++ index.js
@@ -0,0 +1,4 @@
+
+export { youtrackRecheckList } from './lib/youtrackRecheckList.js';
+export { youtrackRecheckListBookmarkletLegacy } from './lib/youtrackRecheckList.legacy.js';
+export { youtrackRecheckListBookmarkletPSpeller } from './lib/youtrackRecheckList.PS.js';
