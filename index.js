1f049e5d831a1cf87642cbc653e1296b79f79b41
Nick Freear <nfreear@yahoo.co.uk>
2025-05-21 20:25:31 +0100

    feat: Add character key shortcuts bookmarklet (#4)

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 
+export { YoutrackHtmlListParser } from './lib/YoutrackHtmlListParser.js';
 export { youtrackRecheckListBookmarklet } from './lib/youtrackRecheckList.marklet.js';
 export { youtrackRecheckList } from './lib/youtrackRecheckList.js';
 export { youtrackRecheckListBookmarkletLegacy } from './lib/youtrackRecheckList.legacy.js';
 export { youtrackRecheckListBookmarkletPSpeller } from './lib/youtrackRecheckList.PS.js';
+export { makeAsyncCharCodeIterator } from './lib/util/charCodeIterator.js';
+export { characterKeyShortcutsBookmarklet } from './lib/characterKeyShortcuts.esm.marklet.js';
+export { characterKeyShortcuts, fireKBEvent } from './lib/characterKeyShortcuts.esm.js';
