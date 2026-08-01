04d4b2fdfb6fa519e916e01ffd08fd82fbcd004c
Nick Freear <nfreear@yahoo.co.uk>
2026-08-01 16:39:29 +0100

    feat: Switch to dark mode in `app.css` stylesheet

--- README.md
+++ README.md
@@ -6,7 +6,9 @@
 A collection of useful bookmarklets.
 
 * [Github: @nfreear/bookmarklets][gh]
+* [License: MIT][mit]
 
 [gh]: https://github.com/nfreear/bookmarklets
 [ci]: https://github.com/nfreear/bookmarklets/actions/workflows/node.js.yml
 [ci-img]: https://github.com/nfreear/bookmarklets/actions/workflows/node.js.yml/badge.svg
+[mit]: https://github.com/nfreear/bookmarklets/blob/master/LICENSE.txt
--- script/httpServer.js
+++ script/httpServer.js
@@ -5,10 +5,11 @@
 const liveServer = require('live-server');
 const PATH = require('path');
 
+const port = 8080; // Was: 9001.
 const root = PATH.resolve(__dirname, '..');
 
 const PARAMS = {
-  port: 9001, // Set the server port. Defaults to 8080.
+  port, // Set the server port. Defaults to 8080.
   host: '0.0.0.0', // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
   root, // Set root directory that's being served. Defaults to cwd.
   open: false, // When false, it won't load your browser by default.
